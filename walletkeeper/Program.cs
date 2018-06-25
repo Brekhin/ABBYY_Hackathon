23e64e4451b05ce1f87152cdb8fca772f860d448
Aleksey Brekhin <brekhin@bk.ru>
2018-06-25 03:19:26 +0300

    Update Program.cs

--- walletkeeper/Program.cs
+++ walletkeeper/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -19,6 +19,9 @@ namespace WalletKeeperBot
 {
     class Program
     {
+        private static Message message;
+        private static double bill = 0;
+        private static readonly TelegramBotClient Bot = new Telegram.Bot.TelegramBotClient(WalletKeeper.Config.API_KEY);
 
         private static String deleteNoDigit(String price)
         {
@@ -28,7 +31,7 @@ namespace WalletKeeperBot
                     sb.Append(price[i]);
             return sb.ToString();
         }
-
+        
         public static string ParseString(string str)
         {
             var splittedString = str.ToLower().Split(new char[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
@@ -63,25 +66,69 @@ namespace WalletKeeperBot
             }
         }
 
-        private static readonly TelegramBotClient Bot = new TelegramBotClient(WalletKeeper.Config.API_KEY);
+        
 
         static void Main(string[] args)
         {
             Bot.OnMessage += BotOnMessageReceived;
             Bot.OnMessage += BotOnPhotoReceived;
+            Bot.OnCallbackQuery += BotOnCallbackQuery;
 
             var me = Bot.GetMeAsync().Result;
             Console.Title = me.Username;
        
             Bot.StartReceiving();
-            BotOnPostReceived();
             Console.ReadLine();
             Bot.StopReceiving();
         }
 
+        private static async void BotOnCallbackQuery(object sender, CallbackQueryEventArgs ev)
+        {
+            if (ev.CallbackQuery.Data == "по категориям")
+            {
+                await Bot.SendTextMessageAsync(message.Chat.Id, DataBaseCon.SelectRowsByCategory((int)message.Chat.Id));
+                await Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id);
+            }
+            if (ev.CallbackQuery.Data == "по датам")
+            {
+                await Bot.SendTextMessageAsync(message.Chat.Id, DataBaseCon.SelectRowsByDates((int)message.Chat.Id));
+                await Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id);
+            }
+            if (ev.CallbackQuery.Data == "Транспорт")
+            {
+                DataBaseCon.InsertAmount((int)message.Chat.Id, bill, "Транспорт");
+                await Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id, "Расходы на транспорт зафиксированы");
+                await Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id);
+            }
+            else if (ev.CallbackQuery.Data == "Мобильная связь")
+            {
+                DataBaseCon.InsertAmount((int)message.Chat.Id, bill, "Мобильная связь");
+                await Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id, "Расходы на мобильную связь зафиксированы");
+                await Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id);
+            }
+            else if (ev.CallbackQuery.Data == "Продукты питания")
+            {
+                DataBaseCon.InsertAmount((int)message.Chat.Id, bill, "Продукты питания");
+                await Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id, "Расходы на продукты питания зафиксированы");
+                await Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id);
+            }
+            else if (ev.CallbackQuery.Data == "Техника")
+            {
+                DataBaseCon.InsertAmount((int)message.Chat.Id, bill, "Техника");
+                await Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id, "Расходы на технику зафиксированы");
+                await Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id);
+            }
+            else if (ev.CallbackQuery.Data == "Другое")
+            {
+                DataBaseCon.InsertAmount((int)message.Chat.Id, bill, "Другое");
+                await Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id, "Расходы на другую категорию зафиксированы");
+                await Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id);
+            }
+        }
+
         private static void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
         {
-            var message = messageEventArgs.Message;
+            message = messageEventArgs.Message;
 
             if (message == null || message.Type != MessageType.TextMessage)
                 return;
@@ -91,35 +138,20 @@ namespace WalletKeeperBot
                 Bot.SendTextMessageAsync(message.Chat.Id, WalletKeeper.Constants.HELP_MESSAGE);
             }
 
-            var keyboard2 = new Telegram.Bot.Types.ReplyMarkups.InlineKeyboardMarkup();
-            keyboard2.InlineKeyboard = new Telegram.Bot.Types.InlineKeyboardButtons.InlineKeyboardButton[][]
-            {
-                    new Telegram.Bot.Types.InlineKeyboardButtons.InlineKeyboardButton[]
-                    {
-                        new KeyboardButton("по категориям"),
-                        new KeyboardButton("по датам"),
-                    },
-            };
-
-            bool flag = true;
+            
             if (message.Text.StartsWith("/spending"))
             {
-                    Bot.OnCallbackQuery += (object sc, Telegram.Bot.Args.CallbackQueryEventArgs ev) =>
-                    {                        
-                        if (ev.CallbackQuery.Data == "по категориям" && flag)
-                        {
-                            Bot.SendTextMessageAsync(message.Chat.Id, DataBaseCon.SelectRowsByCategory((int)message.Chat.Id));
-                            Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id);
-                            flag = !flag;
-                        }
-                        if (ev.CallbackQuery.Data == "по датам" && flag)
-                        {
-                            Bot.SendTextMessageAsync(message.Chat.Id, DataBaseCon.SelectRowsByDates((int)message.Chat.Id));
-                            Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id);
-                            flag = !flag;
-                        }                
-                    };
-                    Bot.SendTextMessageAsync(message.Chat.Id, "Выбери вид отчетности", replyMarkup: keyboard2);
+                var keyboard2 = new Telegram.Bot.Types.ReplyMarkups.InlineKeyboardMarkup();
+                keyboard2.InlineKeyboard = new Telegram.Bot.Types.InlineKeyboardButtons.InlineKeyboardButton[][]
+                {
+                    new Telegram.Bot.Types.InlineKeyboardButtons.InlineKeyboardButton[]
+                    {
+                        Telegram.Bot.Types.InlineKeyboardButtons.InlineKeyboardButton.WithCallbackData("по категориям"),
+                        Telegram.Bot.Types.InlineKeyboardButtons.InlineKeyboardButton.WithCallbackData("по датам"),
+                    },
+                };
+
+                Bot.SendTextMessageAsync(message.Chat.Id, "Выбери вид отчетности", replyMarkup: keyboard2);
             }
 
             if (message.Text.StartsWith("/start"))
@@ -134,30 +166,9 @@ namespace WalletKeeperBot
             }
         }
 
-        private static string API_URL = "http://www.ispeech.org/p/generic/getaudio?text=%TEXT%&voice=rurussianfemale&speed=0&action=convert";
-
-        public static string GetFinalUrl(string text)
-        {
-            return API_URL.Replace("%TEXT%", HttpUtility.UrlEncode(text));
-        }
-
-        public static byte[] GetSpeechFromText(string text)
-        {
-            WebClient wc = new WebClient();
-            try
-            {
-                byte[] b = wc.DownloadData(GetFinalUrl(text));
-                return b;
-            }
-            catch
-            {
-                return null;
-            }
-        }
-
         private static async void BotOnPhotoReceived(object sender, MessageEventArgs messageEventArgs)
         {
-            var message = messageEventArgs.Message;
+            message = messageEventArgs.Message;
             try
             {
                 if (message == null || message.Type != MessageType.PhotoMessage)
@@ -191,7 +202,7 @@ namespace WalletKeeperBot
                     fts.Filename = FileUrl.Split('\\').Last();
                     var test = await Bot.SendStickerAsync(message.Chat.Id, fts);
                 }
-                
+
                 string imagePath = $"../../Photo/img{message.Chat.Id}{fileId}.jpg";
 
                 TextDetection newTD = new TextDetection();
@@ -200,18 +211,7 @@ namespace WalletKeeperBot
 
                 string result = ParseString(text);
                 Console.WriteLine(Convert.ToDouble(result));
-                double result1 = Math.Abs(Convert.ToDouble(result));
-           
-               
-                byte[] byteArray = GetSpeechFromText(result1.ToString());
-                MemoryStream streamMy = new MemoryStream(byteArray);
-
-                await Bot.SendVoiceAsync(message.Chat.Id, new FileToSend("audio.mp3", streamMy));
-
-
-
-                DataBaseCon.InsertUser((int)message.Chat.Id, message.Chat.FirstName);
-                DataBaseCon.InsertAmount((int)message.Chat.Id, result1);
+                bill = Math.Abs(Convert.ToDouble(result));
 
                 var keyboard = new Telegram.Bot.Types.ReplyMarkups.InlineKeyboardMarkup();
                 keyboard.InlineKeyboard = new Telegram.Bot.Types.InlineKeyboardButtons.InlineKeyboardButton[][]
@@ -234,77 +234,13 @@ namespace WalletKeeperBot
 
                 await Bot.SendTextMessageAsync(message.Chat.Id, "Выберите категорию товара, который вы приобрели", replyMarkup: keyboard);
                 
+                await Bot.SendTextMessageAsync(message.Chat.Id, WalletKeeper.Constants.IT_IS_DONE);
 
-                await Task.Delay(1000);
-
-                bool flag = true;
-                           
-                Bot.OnCallbackQuery += (object sc, CallbackQueryEventArgs ev) =>
-                {
-                    Task.Delay(5000);
-                    if (ev.CallbackQuery.Data == "Транспорт" && flag)
-                    {
-                        DataBaseCon.InsertCategory((int)message.Chat.Id, "Транспорт");
-                        Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id, "Расходы на транспорт зафиксированы");
-                        Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id);
-                    }
-                    else
-                    if (ev.CallbackQuery.Data == "Мобильная связь" && flag)
-                    {
-                        DataBaseCon.InsertCategory((int)message.Chat.Id, "Мобильная связь");
-                        Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id, "Расходы на мобильную связь зафиксированы");
-                        Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id);
-                    }
-                    else
-                    if (ev.CallbackQuery.Data == "Продукты питания" && flag)
-                    {
-                        DataBaseCon.InsertCategory((int)message.Chat.Id, "Продукты питания");
-                        Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id, "Расходы на продукты питания зафиксированы");
-                        Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id);
-
-                    }
-                    else
-                    if (ev.CallbackQuery.Data == "Техника" && flag)
-                    {
-                        DataBaseCon.InsertCategory((int)message.Chat.Id, "Техника");
-                        Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id, "Расходы на технику зафиксированы");
-                        Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id);
-                    }
-                    else
-                    if (ev.CallbackQuery.Data == "Другое" && flag)
-                    {
-                        DataBaseCon.InsertCategory((int)message.Chat.Id, "Другое");
-                        Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id, "Расходы на другую категорию зафиксированы");
-                        Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id);
-                    }
-                    flag = !flag;
-                };
-                
-                if (flag)
-                {  
-                    await Bot.SendTextMessageAsync(message.Chat.Id, WalletKeeper.Constants.IT_IS_DONE);
-                }
-                               
             }
-            catch (Exception e){
-                await Bot.SendTextMessageAsync(message.Chat.Id, WalletKeeper.Constants.FAILED);            
-            }
-        }
-
-        private static void BotOnPostReceived()
-        {
-            WebRequest request = WebRequest.Create("https://api.telegram.org/bot460028209:AAHcxwlE3XYAD9TjSWyphmqR_FCMl0QvDLM/sendMessage?chat_id=187064809&text=%D0%97%D0%B4%D0%B5%D1%81%D1%8C%20%D0%BC%D0%BE%D0%B3%D0%BB%D0%B0%20%D0%B1%D1%8B%D1%82%D1%8C%20%D0%B2%D0%B0%D1%88%D0%B0%20%D1%80%D0%B5%D0%BA%D0%BB%D0%B0%D0%BC%D0%B0%20))");
-            request.Method = "POST";
-            string sName = "sName= abracadabra";
-            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(sName);
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.ContentLength = byteArray.Length;
-
-            using (Stream dataStream = request.GetRequestStream())
+            catch (Exception e)
             {
-                dataStream.Write(byteArray, 0, byteArray.Length);
+                await Bot.SendTextMessageAsync(message.Chat.Id, WalletKeeper.Constants.FAILED);
             }
-            
         }
     }
 }
